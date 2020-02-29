b1373c41af05039aa2f4bb346c5a1f1cadded908
chace <morannon1244@gmail.com>
2020-02-28 23:00:59 -0600

    tweaks

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # homework-07
+AS A developer
+
+I WANT a README generator
+
+SO THAT I can easily put together a good README for a new project
--- index.js
+++ index.js
@@ -1,31 +1,7 @@
-//require fs to write file
-//user asked for github username
-//github api call: retrieves email & profile image
-//questions about user's project:
-//project title
-//description
-//installation?
-//Usage?
-//License
-//Contributing : multiple users?
-//tests?
-//questions
-//
-//ToC? : automatic
-//badge specific to the repo
-//user's profile pic
-//user's email
 
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-const util = require("util");
-
-//const writeFileAsync = util.promisify(fs.writeFile);
-
-//function generateMD(answers) {
- //return "Project Title: " + answers.projectTitle +  "\nUsername: " + answers.username + "\nEmail: "
-//}
 
 inquirer
   .prompt([
@@ -39,7 +15,7 @@ inquirer
     name: "projectTitle",
     message: "What is the title of your project?"
   },
-  /*
+  
   {
     type: "input",
     name: "description",
@@ -69,26 +45,26 @@ inquirer
       type: "input",
       name: "tests",
       message: "Tests: "
-  }, */
+  }, 
 ])
   .then(function(answers) {
       const userName = answers.username;
     const queryUrl = `https://api.github.com/users/` + userName;
-    // url for repository: https://api.github.com/users/${username}/repos?per_page=100
-    
+   
     axios.get(queryUrl).then(function(res) { 
         console.log(res.data.email)
         const email = res.data.email
-        const picURL = res.data.avatar_url
+        const picURL = "(" + res.data.avatar_url + ")"
+
       
-       fs.appendFile("info.md", 
-       "Project Title: " + answers.projectTitle + "\nUser: " + answers.username + "\nEmail: " + email + "\n \nTable of Contents: \nInstallation \nUsage \nLicense \nContributing \nTests \n \nInstallation: \n" + answers.installation + "\nUsage: \n" + answers.usage + "\nLicense: \n" + answers.license + "\nContributing: \n" + answers.contributors + "\nTests: \n" + answers.tests + "\n",
+       fs.appendFile("generatedREADME.md", 
+       "Project Title: " + answers.projectTitle + "\n \nTable of Contents: \nInstallation \nUsage \nLicense \nContributing \nTests \n \nInstallation: \n" + answers.installation + "\nUsage: \n" + answers.usage + "\nLicense: \n" + answers.license + "\nContributing: \n" + answers.contributors + "\nTests: \n" + answers.tests + "\n \n \n" + "![User's profile picture]" + picURL + "\n \nUser: " + answers.username + "\nEmail: " + email ,
        function(err) {
             if (err) {
               throw err;
             }
     
-            console.log(`Saved ${picURL}`);
+            console.log(`Saved to generatedREADME`);
           });
       
     });
